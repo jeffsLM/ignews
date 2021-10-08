ed1b0233a6675530041276a5ebb8a66f3f447135
jeffsLM <jeffes.dev@gmail.com>
2021-10-07 21:03:21 -0300

    add create stripe customer

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,24 @@
+import { signin, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
-interface SubscribeButtonProps{
+interface SubscribeButtonProps {
     priceId: string;
 }
 
-export function SubscribeButton({priceId}:SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession()
+
+    function handleSubscribe() {
+        if (!session) {
+            signin('github')
+            return;
+        }
+        
+    }
+
+
     return (
-        <button type="button" className={styles.SubscribeButton}>
+        <button type="button" className={styles.SubscribeButton} onClick={handleSubscribe}>
 
             Subscribe Now
         </button>
